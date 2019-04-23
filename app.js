d65dd3ac2829964ce4a3c8601e210f179f845bb6
mle4d <mle4d@me.com>
2019-04-23 16:26:08 -0700

    element

---
+++ app.js
@@ -0,0 +1,11 @@
+import scoreAnswers from './score=answer.js';
+
+const collectInput = document.getElementById('collect-answer')
+const ageInput = document.getElementById('age-answer')
+const kidsInput = document.getElementById('kids-answer')
+const catsInput = document.getElementById('cats-answer')
+const foodInput = document.getElementById('food-answer')
+const submitButton = document.getElementById('submit-button')
+const quizResult = document.getElementById('quiz-result')
+
+
