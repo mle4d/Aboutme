734e5124611d607d2da25e5cc26d49d70071b0b8
mle4d <mle4d@me.com>
2019-04-24 09:44:28 -0700

    fixed test

--- answer.js
+++ answer.js
@@ -1,30 +1,29 @@
 function scoreAnswers(collectAnswer, ageAnswer, kidsAnswer, catsAnswer, foodAnswer){
 collectAnswer = collectAnswer.toLowerCase();
-ageAnswer = ageAnswer.toLowerCase();
+//ageAnswer = ageAnswer.toLowerCase();
 kidsAnswer = kidsAnswer.toLowerCase();
 catsAnswer = catsAnswer.toLowerCase();
-foodAnswer = foodAnswer.toLowercase();
-}
-let score = 0;
+foodAnswer = foodAnswer.toLowerCase();
 
-    if(collectAnswer === 'pins, my little ponies, stickers') {
+let score = 0;
+console.log('from answer function', ageAnswer)
+    if(collectAnswer === 'pony') {
         score += 1;
     }
     if(ageAnswer === '29') {
         score += 1;
     }
-    if(kidsAnswer === '1, one') {
+    if(kidsAnswer === 'one') {
         score += 1;
     }
-    if(catstAnswer === '2, two') {
+    if(catsAnswer === 'two') {
         score += 1;
     }
-    if(foodAnswer === 'vegan food, vegan') {
+    if(foodAnswer === 'vegan') {
         score += 1;
     }
 
     return score;
 }
 
-export default scoreAnswers;
 export default scoreAnswers;
--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,16 @@
-import scoreAnswers from './src/answers.js';
+import scoreAnswers from '../answer.js';
 const test = QUnit.test;
 
 test('all answers wrong is 0', (assert) => {
 
     const collectAnswer = 'garbage';
-    const ageAnswer = '30';
+    const ageAnswer = 30;
     const kidsAnswer = 'two';
     const catsAnswer = '50';
     const foodAnswer = 'not vegan';
 
     const expected = 0;
-
+console.log('banana')
     const score = scoreAnswers(collectAnswer, ageAnswer, kidsAnswer, catsAnswer, foodAnswer);
    
     assert.equal(score, expected);
