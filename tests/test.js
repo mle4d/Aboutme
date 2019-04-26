dcd749a8f2e589a116e9448e7541e8449399c84f
mle4d <mle4d@me.com>
2019-04-26 14:36:02 -0700

    fixed stuff

--- tests/test.js
+++ tests/test.js
@@ -10,7 +10,7 @@ test('all answers wrong is 0', (assert) => {
     const foodAnswer = 'not vegan';
 
     const expected = 0;
-console.log('banana')
+
     const score = scoreAnswers(collectAnswer, ageAnswer, kidsAnswer, catsAnswer, foodAnswer);
    
     assert.equal(score, expected);
@@ -37,18 +37,5 @@ test('all mixed-case answers right is 5', (assert) => {
 
     const score = scoreAnswers(collectAnswer, ageAnswer, kidsAnswer, catsAnswer, foodAnswer);
     assert.equal(score, expected);
-
-    const message = 'you got ' + score + '/5!';
-
-    quizResult.textContent = mesage;
-
-    if(score <2) {
-        quizResult.classList.add('boo');
-        quizResult.classList.remove('yay');
-    }
-    else {
-        quizResult.classList.add('yay');
-        quizResult.classList.remove('boo');
-    }
 });
 
